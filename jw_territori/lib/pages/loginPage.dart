379fa9af5fdea3d1420982788929ed09ad87acf4
Nenzo99 <126670168+Nenzo99@users.noreply.github.com>
2023-04-24 09:03:47 +0200

    Importate classi

--- jw_territori/lib/pages/loginPage.dart
+++ jw_territori/lib/pages/loginPage.dart
@@ -1,6 +1,10 @@
 // ignore: file_names
+import 'dart:ffi';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
